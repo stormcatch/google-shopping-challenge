fd2dda011a2b60d0ce2a95759567cc3fe8e08d07
zoransjovic <zoransjovic@gmail.com>
2014-11-18 13:50:41 +0100

    remove comment

--- test.php
+++ test.php
@@ -9,24 +9,3 @@ $prices = $crawler->getPrices($ean);
 
 echo "<PRE>", json_encode($prices, JSON_PRETTY_PRINT), "</PRE>";
 
-/*
-    [
-        {
-            "seller": "PDAshop.nl",
-            "price": 179
-        },
-        {
-            "seller": "Belsimpel.nl",
-            "price": 158
-        },
-        {
-            "seller": "bol.com",
-            "price": 189
-        },
-        {
-            "seller": "4Launch",
-            "price": 159.95
-        },
-        ...
-    ]
-*/
