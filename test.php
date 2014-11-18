039b81d4b13900dc0f23c3d38c0bc03c681473f3
zoransjovic <zoransjovic@gmail.com>
2014-11-18 13:53:58 +0100

    added comment

--- test.php
+++ test.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Zoran
+ * Date: 11/13/14
+ * Time: 1:06 PM
+ */
+
 
 require_once 'GoogleShopping.php';
 
 $ean = '8806085553941';
 
+
+// get prices array by ean
 $crawler = new GoogleShopping;
 $prices = $crawler->getPrices($ean);
 
